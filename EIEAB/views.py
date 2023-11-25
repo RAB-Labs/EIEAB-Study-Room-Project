062f2bb80bb5f5dca9dc6702c4db0bf842978040
Ezpert <109883612+Ezpert@users.noreply.github.com>
2023-11-25 13:46:13 -0600

    room info functionality

--- EIEAB/views.py
+++ EIEAB/views.py
@@ -1,26 +1,46 @@
 from django.shortcuts import render
+from .models import bookingRoom
+
+roomInfo = bookingRoom()
 
 
 def home(request):
     return render(request, 'landing.html')
 
 
-def room1(request):
-    return render(request, "Room1Template.html")
 def floor1_view(request):
+    roomInfo.floor = '1'
     return render(request, 'floor1RoomSelect.html')
 
-def room2(request):
-    return render(request, 'Room2Template.html')
 
 def floor2_view(request):
+    roomInfo.floor = '2'
     return render(request, 'floor2RoomSelect.html')
 
-def room3(request):
-    return render(request,'Room3Template.html')
+
 def floor3_view(request):
-    return render(request,'floor3RoomSelect.html')
+    roomInfo.floor = '3'
+    return render(request, 'floor3RoomSelect.html')
+
+
+def room1(request):
+    roomInfo.room = '1.100'
+    return render(request, "Room1Template.html")
+
+
+def room2(request):
+    roomInfo.room = '2.200'
+    return render(request, 'Room2Template.html')
+
+
+def room3(request):
+    roomInfo.room = '3.000'
+    return render(request, 'Room3Template.html')
+
 
 def reserve_view(request):
-    return render(request,'BookingDetails.html')
-
+    room_value = roomInfo.room
+    roomval, created = bookingRoom.objects.get_or_create(room=room_value)
+    if created:
+        roomInfo.save()
+    return render(request, 'BookingDetails.html', {'roomInfo': roomInfo})
